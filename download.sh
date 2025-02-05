68a0ce252ecd6b7a66d6fb17722af380d12015d8
Taryx <taryx@proton.me>
2025-02-06 02:11:07 +0800

    fix(deps): downgrade agent to 1.2.0

--- download.sh
+++ download.sh
@@ -5,7 +5,8 @@ CF_VERSION="$(curl -s https://api.github.com/repos/cloudflare/cloudflared/releas
 wget -q https://github.com/cloudflare/cloudflared/releases/download/${CF_VERSION}/cloudflared-linux-amd64
 mv  cloudflared-linux-amd64 cloudflared
 
-AGENT_VERSION="$(curl -s https://api.github.com/repos/nezhahq/agent/releases | grep -m 1 -oP '"tag_name":\s*"v\K[^"]+')"
+#AGENT_VERSION="$(curl -s https://api.github.com/repos/nezhahq/agent/releases | grep -m 1 -oP '"tag_name":\s*"v\K[^"]+')"
+AGENT_VERSION="v1.2.0"
 
 wget -q https://github.com/nezhahq/agent/releases/download/v${AGENT_VERSION}/nezha-agent_linux_amd64.zip
 unzip   nezha-agent_linux_amd64.zip
